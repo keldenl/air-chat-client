fe77cc5a811f5303f0e9f62adb120399840ac76b
keldenl <keldenlin@gmail.com>
2022-06-17 22:18:54 -0700

    copy pasta some data channel stuff, add input and selection for user to send data to

--- src/App.js
+++ src/App.js
@@ -5,15 +5,35 @@ import { MyUserProfile } from './components/MyUserProfile';
 
 import './App.css';
 
+// Use for remote connections
+// const configuration = {
+//   iceServers: [{ url: "stun:stun.1.google.com:19302" }]
+// };
+
+// Use for local connections
+const configuration = null;
+
 function App() {
   // WebSocket State
   const webSocket = useRef(null);
+  const connectedRef = useRef();
   const [socketOpen, setSocketOpen] = useState(false);
   const [socketMessages, setSocketMessages] = useState([]);
+
+  const messagesRef = useRef({});
+  const [messages, setMessages] = useState({});
+
+  const [connectedTo, setConnectedTo] = useState();
+  const [connection, setConnection] = useState();
+  const [channel, setChannel] = useState();
 
   // App State
   const [myData, setMyData] = useState({});
   const [users, setUsers] = useState([]);
+  const [input, setInput] = useState('');
+  const [offerTo, setOfferTo] = useState();
+
+  const send = data => webSocket.current.send(JSON.stringify(data));
 
   // All WebSocket Handlers
   const handleConnect = (message) => {
@@ -29,6 +49,30 @@ function App() {
       return;
     }
     setUsers(users);
+    let localConnection = new RTCPeerConnection(configuration);
+
+    // when the browser finds an ice candidate we send it to another peer
+    localConnection.onicecandidate = ({ candidate }) => {
+      let connectedTo = connectedRef.current;
+
+      if (candidate && !!connectedTo) {
+        send({
+          name: connectedTo,
+          type: "candidate",
+          candidate
+        });
+      }
+    };
+    localConnection.ondatachannel = event => {
+      console.log("Data channel is created!");
+      let receiveChannel = event.channel;
+      receiveChannel.onopen = () => {
+        console.log("Data channel is open and ready to be used.");
+      };
+      receiveChannel.onmessage = handleDataChannelMessageReceived;
+      setChannel(receiveChannel);
+    };
+    setConnection(localConnection);
   }
 
   const handleUpdateUser = (message) => {
@@ -41,6 +85,53 @@ function App() {
     const updatedUserList = users.filter(user => user.id !== leaveUser.id);
     setUsers(updatedUserList);
   }
+
+  const handleDataChannelMessageReceived = ({ data }) => {
+    const message = JSON.parse(data);
+    const { name: user } = message;
+    let messages = messagesRef.current;
+    let userMessages = messages[user];
+    if (userMessages) {
+      userMessages = [...userMessages, message];
+      let newMessages = Object.assign({}, messages, { [user]: userMessages });
+      messagesRef.current = newMessages;
+      setMessages(newMessages);
+    } else {
+      let newMessages = Object.assign({}, messages, { [user]: [message] });
+      messagesRef.current = newMessages;
+      setMessages(newMessages);
+    }
+  };
+
+  //when somebody wants to message us
+  const handleOffer = ({ offer, name }) => {
+    setConnectedTo(name);
+    connectedRef.current = name;
+
+    connection
+      .setRemoteDescription(new RTCSessionDescription(offer))
+      .then(() => connection.createAnswer())
+      .then(answer => connection.setLocalDescription(answer))
+      .then(() =>
+        send({ type: "answer", answer: connection.localDescription, name })
+      )
+      .catch(e => {
+        console.log({ e });
+        window.alert('An error has occurred.'
+        );
+      });
+  };
+
+  //when another user answers to our offer
+  const onAnswer = ({ answer }) => {
+    connection.setRemoteDescription(new RTCSessionDescription(answer));
+  };
+
+  //when we got ice candidate from another user
+  const onCandidate = ({ candidate }) => {
+    connection.addIceCandidate(new RTCIceCandidate(candidate));
+  };
+
 
   useEffect(() => {
     // add the websocket url to env in production environment     
@@ -87,7 +178,7 @@ function App() {
   const usersDisplay = users.length > 0 ?
     <ul>
       {users.map(user => <li key={user.id}>
-        <User data={user} />
+        <User data={user} offerTo={offerTo} setOfferTo={setOfferTo} />
       </li>)}
 
     </ul> :
@@ -97,6 +188,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">Air Chat</header>
+      <input type="text" value={input} onChange={e => setInput(e.target.value)} />
       <button onClick={sendMessage}>Send Message</button>
       {socketOpen ? <p>Socket opened</p> : undefined}
       <div>
--- src/components/User/index.jsx
+++ src/components/User/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import "./style.css";
 
-export function User({ data }) {
+export function User({ data, offerTo, setOfferTo }) {
     const { id, ip, userName } = data;
+
+    const onSetOfferTo = () => {
+        setOfferTo(id);
+    }
+
     return (
-        <div>
+        <div onClick={onSetOfferTo} className={`${offerTo === id ? 'user-selected' : ''} user`}>
             <p>{userName}</p>
         </div>
     )
