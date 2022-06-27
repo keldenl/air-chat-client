42219b5720d403540d19304ea892cadaa9ea2ab4
keldenl <keldenlin@gmail.com>
2022-06-26 22:05:59 -0700

    add actual connection, added error handling when leaving

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
   const [messages, setMessages] = useState({});
 
   const [connectedTo, setConnectedTo] = useState();
+  const [isConnected, setIsConnected] = useState(false);
   const [connection, setConnection] = useState();
   const [channel, setChannel] = useState();
 
@@ -34,6 +35,11 @@ function App() {
   const [offerTo, setOfferTo] = useState();
 
   const send = data => webSocket.current.send(JSON.stringify(data));
+
+  const onConnectedUserLeave = () => {
+    setConnectedTo(undefined);
+    setIsConnected(false);
+  }
 
   // All WebSocket Handlers
   const handleConnect = (message) => {
@@ -72,6 +78,7 @@ function App() {
       receiveChannel.onmessage = handleDataChannelMessageReceived;
       setChannel(receiveChannel);
     };
+    console.log('set connectino to ', localConnection)
     setConnection(localConnection);
   }
 
@@ -88,6 +95,7 @@ function App() {
 
   const handleDataChannelMessageReceived = ({ data }) => {
     const message = JSON.parse(data);
+    console.log(message);
     const { name: user } = message;
     let messages = messagesRef.current;
     let userMessages = messages[user];
@@ -110,13 +118,15 @@ function App() {
     const offerReq = window.confirm(`${name} has offered ${offer}. Do you want to accept?`)
 
     if (offerReq) {
+      console.log('offer: ', offer)
+      console.log('connection: ', connection)
       connection
         .setRemoteDescription(new RTCSessionDescription(offer))
         .then(() => connection.createAnswer())
         .then(answer => connection.setLocalDescription(answer))
         .then(() =>
           send({ type: "answer", answer: connection.localDescription, name })
-        )
+        ).then(() => setIsConnected(true))
         .catch(e => {
           console.log({ e });
           window.alert('An error has occurred.'
@@ -126,13 +136,14 @@ function App() {
   };
 
   //when another user answers to our offer
-  const onAnswer = ({ answer }) => {
+  const handleAnswer = ({ answer }) => {
     connection.setRemoteDescription(new RTCSessionDescription(answer));
   };
 
   //when we got ice candidate from another user
-  const onCandidate = ({ candidate }) => {
+  const handleCandidate = ({ candidate }) => {
     connection.addIceCandidate(new RTCIceCandidate(candidate));
+    setIsConnected(true);
   };
 
 
@@ -167,6 +178,12 @@ function App() {
       case "offer":
         handleOffer(message);
         break;
+      case "answer":
+        handleAnswer(message);
+        break;
+      case "candidate":
+        handleCandidate(message);
+        break;
       case "leave":
         handleLeaveUser(message);
         break;
@@ -176,10 +193,63 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [socketMessages])
 
-  const sendMessage = () => {
-    const data = { type: "offer", name: offerTo, offer: input }
-    send(data);
-  }
+  const handleConnection = () => {
+    var dataChannelOptions = {
+      reliable: true
+    };
+
+    let dataChannel = connection.createDataChannel("messenger");
+
+    dataChannel.onerror = e => {
+      console.error(e.error.message);
+      if (e.error.message === 'User-Initiated Abort, reason=Close called') {
+        onConnectedUserLeave();
+      } else {
+        window.alert(e.error.message);
+      }
+    }
+
+    dataChannel.onmessage = handleDataChannelMessageReceived;
+    setChannel(dataChannel);
+    setConnectedTo(offerTo)
+
+    connection
+      .createOffer()
+      .then(offer => connection.setLocalDescription(offer))
+      .then(() =>
+        send({ type: "offer", name: offerTo, offer: connection.localDescription })
+      )
+      .catch(e => {
+        console.error(e);
+        window.alert('handleconnection', e)
+      });
+  };
+
+  useEffect(() => {
+    console.log(connectedTo)
+  }, [connectedTo])
+
+  //when a user clicks the send message button
+  const sendMsg = () => {
+    let text = { message: input, name: myData.name };
+    let messages = messagesRef.current;
+    let connectedTo = connectedRef.current;
+    let userMessages = messages[connectedTo];
+    if (messages[connectedTo]) {
+      userMessages = [...userMessages, text];
+      let newMessages = Object.assign({}, messages, {
+        [connectedTo]: userMessages
+      });
+      messagesRef.current = newMessages;
+      setMessages(newMessages);
+    } else {
+      userMessages = Object.assign({}, messages, { [connectedTo]: [text] });
+      messagesRef.current = userMessages;
+      setMessages(userMessages);
+    }
+    channel.send(JSON.stringify(text));
+    setInput("");
+  };
 
   const usersDisplay = users.length > 0 ?
     <ul>
@@ -194,13 +264,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">Air Chat</header>
-      <input type="text" value={input} onChange={e => setInput(e.target.value)} />
-      <button onClick={sendMessage}>Send Message</button>
       {socketOpen ? <p>Socket opened</p> : undefined}
       <div>
         <h2>Nearby Devices</h2>
         {usersDisplay}
       </div>
+      {isConnected ? <p>Connected to {connectedTo}</p> : undefined}
+      <button onClick={handleConnection}>Request Connection</button>
+      {isConnected ? (
+        <div>
+          <input type="text" value={input} onChange={e => setInput(e.target.value)} />
+          <button onClick={sendMsg} disabled={!isConnected}>Send</button>
+        </div>
+      ) : undefined}
       <MyUserProfile myData={myData} />
     </div>
   );
